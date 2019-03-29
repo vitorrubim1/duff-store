15d167ec90808e846e483017e94ee08ae455b7f6
VITOR RUBIM PASSOS <52292234X@alunos.etec.itaqua>
2019-03-28 22:26:57 -0300

    Implementado passagem de parametros pela url para outras paginas

--- src/app/cerveja-detalhes/cerveja-detalhes.page.ts
+++ src/app/cerveja-detalhes/cerveja-detalhes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cerveja-detalhes',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cerveja-detalhes.page.scss'],
 })
 export class CervejaDetalhesPage implements OnInit {
+  
+  nome;
+  detalhes;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) {
+    console.log("EXECUTOU O CONSTRUTOR")
+   }
 
   ngOnInit() {
+    let idcerveja = this.route.snapshot.params.id;
+    if(idcerveja === '50') {
+      this.nome ='Proibida'
+      this.detalhes ='Tão gostosa que é Proibida, A melhor cerveja do ceará'
+    } else if (idcerveja === '70'){
+      this.nome ='Skol'
+      this.detalhes ='A Puro Malte que desce rendondo'
+    }
   }
+
+
 
 }
